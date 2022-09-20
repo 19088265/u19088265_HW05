815f824ea151e01d9356c3afbcd56d87eb26c0ad
Gontse Setlaleleng <u19088265@tuks.co.za>
2022-09-20 14:04:25 +0200

    Created student view

--- HW05/App_Start/RouteConfig.cs
+++ HW05/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HW05
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "BookDetails", id = UrlParameter.Optional }
             );
         }
     }
--- HW05/Controllers/HomeController.cs
+++ HW05/Controllers/HomeController.cs
@@ -13,16 +13,8 @@ namespace HW05.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult BookDetails()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }

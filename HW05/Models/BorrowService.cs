69b303285404a4b3760c7a18eade596296cda4ca
19088265 <103127317+19088265@users.noreply.github.com>
2022-10-05 19:19:49 +0200

    finalize

---
+++ HW05/Models/BorrowService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using HW05.Models;
+
+namespace HW05.Models
+{
+    public class BorrowService
+    {
+        public string stuName { get; set; }
+        public Nullable<int> borrowID { get; set; }
+        public Nullable<int> takenDate { get; set; }
+        public Nullable<int> broughtDate { get; set; }
+    
+    }
+}
---
+++ HW05/Models/StudentsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW05.Models
+{
+    public class StudentsViewModel
+    {
+        public int sID { get; set; }
+        public string sName { get; set; }
+        public string sSurname { get; set; }
+        public string sClass { get; set; }
+        public int sPoint { get; set; }
+    }
+}
